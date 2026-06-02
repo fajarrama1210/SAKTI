f83b281459d95c0092cfdd294a813b380cd256b4
Fajar Ramadhan <jarrr1210@gmail.com>
2026-06-02 11:46:58 +0700

    feat: force HTTPS scheme in production environment within AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         \Illuminate\Pagination\Paginator::useBootstrapFive();
 
         // Share unpaid bills count to student bottom nav
