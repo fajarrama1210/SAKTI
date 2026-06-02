20366449e4f42f592156f28e7acdd0b6a907cc12
Fajar Ramadhan <jarrr1210@gmail.com>
2026-06-02 11:09:46 +0700

    refactor: use forceCreate and add email verification fields in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,23 +15,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
+        \App\Models\User::forceCreate([
             'name' => 'admin',
             'email' => 'asd@asd.asd',
             'password' => bcrypt('asdasd'),
             'role' => 'admin',
+            'email_verified_at' => now(),
+            'remember_token' => \Illuminate\Support\Str::random(10),
         ]);
         \App\Models\User::factory()->create([
             'name' => 'mulyono',
             'email' => 'E3125mulyono@ac.id',
             'password' => bcrypt('mulyono'),
             'role' => 'admin',
+            'email_verified_at' => now(),
+            'remember_token' => \Illuminate\Support\Str::random(10),
         ]);
-        \App\Models\User::factory()->create([
+        \App\Models\User::forceCreate([
             'name' => 'Kepala Sekolah SAKTI',
             'email' => 'kepala@sekolah.id',
             'password' => bcrypt('kepala123'),
             'role' => 'kepala_sekolah',
+            'email_verified_at' => now(),
+            'remember_token' => \Illuminate\Support\Str::random(10),
         ]);
     }
 }
